99dc6768c964d99a18a6fdccd4dd43f6bd301f3f
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-05 17:30:47 +0545

    train and val folder separated along with requirements

---
+++ ai/face_recog/face_recog_test.py
@@ -0,0 +1,8 @@
+from os import listdir
+from os.path import isdir
+from PIL import Image
+from matplotlib import pyplot
+from numpy import savez_compressed
+from numpy import asarray
+from mtcnn.mtcnn import MTCNN
+
