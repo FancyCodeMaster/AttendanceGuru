4f50db01b9d02eb385f1a661595f18eff74957b8
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-06 23:22:21 +0545

    face recognition with FaceNet Successful

--- ai/face_recog/face_recog_test.py
+++ ai/face_recog/face_recog_test.py
@@ -74,3 +74,128 @@ print(trainy)
 testX, testy = load_dataset('./FaceData/val')
 # save arrays to one file in compressed format
 savez_compressed('faces-dataset.npz', trainX, trainy, testX, testy)
+
+# calculate a face embedding for each face in the dataset using facenet
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
+from keras_facenet import FaceNet
+
+# get the face embedding for one face
+def get_embedding(model, face_pixels):
+	# scale pixel values
+	face_pixels = face_pixels.astype('float32')
+	# standardize pixel values across channels (global)
+	# mean, std = face_pixels.mean(), face_pixels.std()
+	# face_pixels = (face_pixels - mean) / std
+  # detections = model.extract(face_pixels, threshold=0.95)
+	# transform face into one sample
+	samples = expand_dims(face_pixels, axis=0)
+	# make prediction to get embedding
+	yhat = model.embeddings(samples)
+	return yhat[0]
+
+# load the face dataset
+print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
+# load the facenet model
+model = FaceNet()
+print('Loaded Model')
+# convert each face in the train set to an embedding
+newTrainX = list()
+for face_pixels in trainX:
+	embedding = get_embedding(model, face_pixels)
+	newTrainX.append(embedding)
+newTrainX = asarray(newTrainX)
+print(newTrainX.shape)
+# convert each face in the test set to an embedding
+newTestX = list()
+for face_pixels in testX:
+	embedding = get_embedding(model, face_pixels)
+	newTestX.append(embedding)
+newTestX = asarray(newTestX)
+print(newTestX.shape)
+# save arrays to one file in compressed format
+savez_compressed('faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
+
+# develop a classifier for the 5 Celebrity Faces Dataset
+from numpy import load
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+# load dataset
+data = load('faces-embeddings.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Dataset: train=%d, test=%d' % (trainX.shape[0], testX.shape[0]))
+# normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
+testX = in_encoder.transform(testX)
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+testy = out_encoder.transform(testy)
+# fit model
+model = SVC(kernel='linear', probability=True)
+model.fit(trainX, trainy)
+# predict
+yhat_train = model.predict(trainX)
+yhat_test = model.predict(testX)
+# score
+score_train = accuracy_score(trainy, yhat_train)
+score_test = accuracy_score(testy, yhat_test)
+# summarize
+print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
+
+# develop a classifier for the 5 Celebrity Faces Dataset
+from random import choice
+from numpy import load
+from numpy import expand_dims
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+from matplotlib import pyplot
+# load faces
+data = load('faces-dataset.npz')
+testX_faces = data['arr_2']
+# load face embeddings
+data = load('faces-embeddings.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+# normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
+testX = in_encoder.transform(testX)
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+testy = out_encoder.transform(testy)
+# fit model
+model = SVC(kernel='linear', probability=True)
+model.fit(trainX, trainy)
+# test model on a random example from the test dataset
+# selection = choice([i for i in range(testX.shape[0])])
+selection = choice([1])
+print(testX.shape[0])
+random_face_pixels = testX_faces[selection]
+random_face_emb = testX[selection]
+random_face_class = testy[selection]
+random_face_name = out_encoder.inverse_transform([random_face_class])
+# prediction for the face
+samples = expand_dims(random_face_emb, axis=0)
+yhat_class = model.predict(samples)
+yhat_prob = model.predict_proba(samples)
+# get name
+class_index = yhat_class[0]
+class_probability = yhat_prob[0,class_index] * 100
+predict_names = out_encoder.inverse_transform(yhat_class)
+print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+print('Expected: %s' % random_face_name[0])
+# plot for fun
+pyplot.imshow(random_face_pixels)
+title = '%s (%.3f)' % (predict_names[0], class_probability)
+pyplot.title(title)
+pyplot.show()
