2c9225aa780df328a92f8c60b9b57f800e941873
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-08 22:21:55 +0545

    desktop app frontend updated

--- desktop-application/frontend/src/main.jsx
+++ desktop-application/frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './Pages/ErrorPage/ErrorPage.jsx';
+
+
+import { AuthProvider } from './context/AuthProvider';
+
+
+const router = createBrowserRouter([
+  {
+    path : "*",
+    element : <ErrorPage />,
+    errorElement : <ErrorPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>,
+)
