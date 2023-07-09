e910ef598dfc1639235869bab04c59acd5a5244c
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-09 23:04:05 +0545

    tailwind config file updated

--- desktop-application/frontend/tailwind.config.js
+++ desktop-application/frontend/tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        login : "url('src/assests/Images/login.jpg')",
+        teacherAvatar : "url('src/assets/Images/teacher-avatar.jpg')",
+        studentAvatar : "url('src/assets/Images/student-avatar.jpg')",
+        homepage : "url('src/assets/Images/homepage.jpg)",
+      },
+      colors : {
+        kellyGreen : '#4cbb17',
+        neonBlue : '#4d4dff',
+        discordBlue : '#5765f2',
+        discordBlack : '#37393f',
+        discordWhite : '#f1f1f1',
+        discordDarkBlack : '#202225'
+      }
+    },
   },
   plugins: [],
 }
