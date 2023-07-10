494f8d74ddbcdcae9987b73872d3cd5004ff2c02
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-10 19:29:42 +0545

    test file added

---
+++ ai/test.py
@@ -0,0 +1,24 @@
+# test model on a random example from the test dataset
+# selection = choice([i for i in range(testX.shape[0])])
+selection = choice([6])
+print(testX.shape[0])
+random_face_pixels = testX_faces[selection]
+random_face_emb = testX[selection]
+random_face_class = testy[selection]
+random_face_name = out_encoder.inverse_transform([random_face_class])
+# prediction for the face
+samples = expand_dims(random_face_emb, axis=0)
+yhat_class = model.predict(samples)
+print(yhat_class)
+yhat_prob = model.predict_proba(samples)
+# get name
+class_index = yhat_class[0]
+class_probability = yhat_prob[0,class_index] * 100
+predict_names = out_encoder.inverse_transform(yhat_class)
+print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+print('Expected: %s' % random_face_name[0])
+# plot for fun
+pyplot.imshow(random_face_pixels)
+title = '%s (%.3f)' % (predict_names[0], class_probability)
+pyplot.title(title)
+pyplot.show()
