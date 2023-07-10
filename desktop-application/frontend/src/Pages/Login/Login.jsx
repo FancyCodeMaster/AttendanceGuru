eeceab81916d503b1e546a88a73bfcc0675bd56f
FancyCodeMaster <fancycodemaster@gmail.com>
2023-07-10 16:15:23 +0545

    configuration updated

--- desktop-application/frontend/src/Pages/Login/Login.jsx
+++ desktop-application/frontend/src/Pages/Login/Login.jsx
@@ -9,102 +9,106 @@ import AuthContext from '../../context/AuthProvider';
 import Bars from 'react-loading-icons/dist/esm/components/bars';
 
 const Login = () => {
-  const {setAuth} = useContext(AuthContext);
-  const firstInputBoxRef = useRef();
+  // const {setAuth} = useContext(AuthContext);
+  // const firstInputBoxRef = useRef();
 
-  const [email, updateEmail] = useState(null);
-  const [password, updatePassword] = useState(null);
-  const [errMsg, setErrMsg] = useState('');
-  const [isButtonLoading, setIsButtonLoading] = useState(false);
+  // const [email, updateEmail] = useState(null);
+  // const [password, updatePassword] = useState(null);
+  // const [errMsg, setErrMsg] = useState('');
+  // const [isButtonLoading, setIsButtonLoading] = useState(false);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    firstInputBoxRef.current.focus();
-  }, [])
+  // useEffect(() => {
+  //   firstInputBoxRef.current.focus();
+  // }, [])
 
-  const checkPattern = (pattern , str) => {
-    let search = str.search(pattern);
-    return search;
-  }
+  // const checkPattern = (pattern , str) => {
+  //   let search = str.search(pattern);
+  //   return search;
+  // }
 
-  const onButtonClick = (e) => {
-    let emailPattern = /^([a-z_A-Z]+)([0-9]*)(\.?)([0-9]*)(\@)([a-z_A-Z]+)(\.)([a-z_A-Z]+)(\.?)([a-z_A-Z]*)$/;
+  // const onButtonClick = (e) => {
+  //   let emailPattern = /^([a-z_A-Z]+)([0-9]*)(\.?)([0-9]*)(\@)([a-z_A-Z]+)(\.)([a-z_A-Z]+)(\.?)([a-z_A-Z]*)$/;
 
-    let emailSearch = checkPattern(emailPattern, email)
-    e.preventDefault();
-    if(email !== '' && password !== '' && emailSearch === 0 && password.length>=8 && password.length<=24){
-      postData();
-    }else{
-      setErrMsg("Invalid User")
-    }
+  //   let emailSearch = checkPattern(emailPattern, email)
+  //   e.preventDefault();
+  //   if(email !== '' && password !== '' && emailSearch === 0 && password.length>=8 && password.length<=24){
+  //     postData();
+  //   }else{
+  //     setErrMsg("Invalid User")
+  //   }
 
-    if(password.length < 8){
-        setErrMsg('Passwords must be at least of 8 characters');
-    }
-    if(password.length > 24){
-        setErrMsg('Passwords must be at most of 8 characters');
-    }
-  }
+  //   if(password.length < 8){
+  //       setErrMsg('Passwords must be at least of 8 characters');
+  //   }
+  //   if(password.length > 24){
+  //       setErrMsg('Passwords must be at most of 8 characters');
+  //   }
+  // }
 
-  const postData = async () => {
-      setErrMsg('');
-      setIsButtonLoading(true);
-      const formData = {
-          email : email,
-          password : password,
-      }
+  // const postData = async () => {
+  //     setErrMsg('');
+  //     setIsButtonLoading(true);
+  //     const formData = {
+  //         email : email,
+  //         password : password,
+  //     }
 
-      try{
-          const response = await axios.post("/signIn", 
-              JSON.stringify(formData), 
-              {
-                  headers : {'Content-Type' : 'application/json'},
-                  withCredentials : true
-              }
-          );
-          const data = await response.data;
-          console.log(data);
-          if(data.role === 'student'){
-            localStorage.setItem("studentStatus", 1);
-            navigate('/dashboard/student')
-          }else if(data.role === 'teacher'){
-            localStorage.setItem("teacherStatus", 1);
-            navigate('/dashboard/teacher')
-          }else{
-            setErrMsg(data.message);
-          }
-          setIsButtonLoading(false);
-          // navigate('/dashboard/student');
-      }catch(error){
-          if (!error?.response){
-              setErrMsg('No Server Response');
-          }else if (error.response?.status === 400){
-              setErrMsg('Missing Email or Password');
-          }else if (error.response?.status === 401) {
-              setErrMsg('Unauthorized');
-          }else {
-              setErrMsg('Login Failed');
-          }
-          setIsButtonLoading(false);
-      }
-  }
+  //     try{
+  //         const response = await axios.post("/signIn", 
+  //             JSON.stringify(formData), 
+  //             {
+  //                 headers : {'Content-Type' : 'application/json'},
+  //                 withCredentials : true
+  //             }
+  //         );
+  //         const data = await response.data;
+  //         console.log(data);
+  //         if(data.role === 'student'){
+  //           localStorage.setItem("studentStatus", 1);
+  //           navigate('/dashboard/student')
+  //         }else if(data.role === 'teacher'){
+  //           localStorage.setItem("teacherStatus", 1);
+  //           navigate('/dashboard/teacher')
+  //         }else{
+  //           setErrMsg(data.message);
+  //         }
+  //         setIsButtonLoading(false);
+  //         // navigate('/dashboard/student');
+  //     }catch(error){
+  //         if (!error?.response){
+  //             setErrMsg('No Server Response');
+  //         }else if (error.response?.status === 400){
+  //             setErrMsg('Missing Email or Password');
+  //         }else if (error.response?.status === 401) {
+  //             setErrMsg('Unauthorized');
+  //         }else {
+  //             setErrMsg('Login Failed');
+  //         }
+  //         setIsButtonLoading(false);
+  //     }
+  // }
 
 
     
   return (
-    <div class='bg-cover bg-center bg-no-repeat bg-fixed md:h-screen md:w-screen md:flex md:justify-center md:items-center' style={{backgroundImage : `url(${background})`}}>
-      <div class='h-screen w-screen bg-discordBlack text-discordWhite py-10 px-6  md:h-auto md:w-1/3'>
-        <h1 class='text-center text-2xl'>Welcome again!</h1>
-        <h2 class='mb-4 text-center text-lg'>Glad to see you again</h2>
-        {(errMsg)?<p class='my-3 text-center text-red-500'>{errMsg}</p>:<p class='my-3 text-center text-red-500 invisible'>this is something</p>}
-        <p class='uppercase hidden md:block'>Email</p>
-        <input type="email" class='bg-discordDarkBlack w-full placeholder:uppercase mb-3 p-2 outline-none md:placeholder:opacity-0' required placeholder='email' onChange={(el) => {updateEmail(el.target.value); setErrMsg('');}}  ref={firstInputBoxRef}></input>
-        <p class='uppercase hidden md:block'>Password</p>
-        <input type="password" class='bg-discordDarkBlack w-full placeholder:uppercase mb-3 p-2 outline-none md:placeholder:opacity-0' required placeholder='password' onChange={(el) => {updatePassword(el.target.value); setErrMsg('');}}></input>
-        <p class='mb-4'>Not a user, <span class='text-discordBlue underline'><Link to="/signup">Sign Up</Link></span></p>
-        <button class='bg-discordBlue text-discordWhite border-discordBlue rounded-none w-full p-2 flex justify-center h-10' onClick={onButtonClick}>{(isButtonLoading)?<Bars class='w-5 h-5' style={{width : '20px', height:'20px'}} />:"Login"}</button>
-      </div>
+    // <div class='bg-cover bg-center bg-no-repeat bg-fixed md:h-screen md:w-screen md:flex md:justify-center md:items-center' style={{backgroundImage : `url(${background})`}}>
+    //   <div class='h-screen w-screen bg-discordBlack text-discordWhite py-10 px-6  md:h-auto md:w-1/3'>
+    //     <h1 class='text-center text-2xl'>Welcome again!</h1>
+    //     <h2 class='mb-4 text-center text-lg'>Glad to see you again</h2>
+    //     {(errMsg)?<p class='my-3 text-center text-red-500'>{errMsg}</p>:<p class='my-3 text-center text-red-500 invisible'>this is something</p>}
+    //     <p class='uppercase hidden md:block'>Email</p>
+    //     <input type="email" class='bg-discordDarkBlack w-full placeholder:uppercase mb-3 p-2 outline-none md:placeholder:opacity-0' required placeholder='email' onChange={(el) => {updateEmail(el.target.value); setErrMsg('');}}  ref={firstInputBoxRef}></input>
+    //     <p class='uppercase hidden md:block'>Password</p>
+    //     <input type="password" class='bg-discordDarkBlack w-full placeholder:uppercase mb-3 p-2 outline-none md:placeholder:opacity-0' required placeholder='password' onChange={(el) => {updatePassword(el.target.value); setErrMsg('');}}></input>
+    //     <p class='mb-4'>Not a user, <span class='text-discordBlue underline'><Link to="/signup">Sign Up</Link></span></p>
+    //     <button class='bg-discordBlue text-discordWhite border-discordBlue rounded-none w-full p-2 flex justify-center h-10' onClick={onButtonClick}>{(isButtonLoading)?<Bars class='w-5 h-5' style={{width : '20px', height:'20px'}} />:"Login"}</button>
+    //   </div>
+    // </div>
+
+    <div>
+      <h1 class='bg-red-500 text-red-500'>This is login</h1>
     </div>
   )
 }
--- desktop-application/frontend/tailwind.config.js
+++ desktop-application/frontend/tailwind.config.js
@@ -1,28 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */ 
+export default { 
+  content: [ 
+    "./src/**/*.{js,jsx}", 
+  ], 
   darkMode: 'class', 
-  theme: {
-    extend: {
-      backgroundImage : {
-        login : "url('src/assests/Images/login.jpg')",
-        teacherAvatar : "url('src/assets/Images/teacher-avatar.jpg')",
-        studentAvatar : "url('src/assets/Images/student-avatar.jpg')",
-        homepage : "url('src/assets/Images/homepage.jpg)",
-      },
-      colors : {
-        kellyGreen : '#4cbb17',
-        neonBlue : '#4d4dff',
-        discordBlue : '#5765f2',
-        discordBlack : '#37393f',
-        discordWhite : '#f1f1f1',
-        discordDarkBlack : '#202225'
-      }
-    },
-  },
-  plugins: [],
-}
-
+  theme: { 
+    extend: { 
+      backgroundImage : { 
+        login : "url('src/assests/Images/login.jpg')", 
+        teacherAvatar : "url('src/assets/Images/teacher-avatar.jpg')", 
+        studentAvatar : "url('src/assets/Images/student-avatar.jpg')", 
+        homepage : "url('src/assets/Images/homepage.jpg)", 
+      }, 
+      colors : { 
+        kellyGreen : '#4cbb17', 
+        neonBlue : '#4d4dff', 
+        discordBlue : '#5765f2', 
+        discordBlack : '#37393f', 
+        discordWhite : '#f1f1f1', 
+        discordDarkBlack : '#202225' 
+      } }, }, 
+      plugins: [], }
